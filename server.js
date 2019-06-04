20766f62fc7b15b9643ae994882651536f2a894c
Steve <stevegojkov@gmail.com>
2019-06-04 19:24:08 -0400

    server

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'Cracky20',
-    database : 'smartbrain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
@@ -31,6 +29,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
